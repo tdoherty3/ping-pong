4901462041ed4e57b775cc79ef44ff14acc5bfa0
Thomas Doherty <thomas@selfsustain.com>
2018-03-10 16:37:31 -0800

    Add js for basic form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
-
-
-var number parseInt(inputtedNumber)
-
-    event.preventDefault();
+$("form#ping-pong").submit(function(event) {
+  event.preventDefault();
+var number = parseInt($("input#number").val());
+console.log(number)
+var result = number;
+$("#result").text(result);
   });
 });
