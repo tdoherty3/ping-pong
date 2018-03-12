254d5d6f0fa1018b584e8b59ae6616b7d57d8e3e
Thomas Doherty <thomas@selfsustain.com>
2018-03-11 23:14:52 -0700

    Try various ways to use Else / If statements to create ping and pong output. Style results using css. Try various ways to sort array into an ordered list.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,52 @@ console.log(number)
 var array = [ ];
 console.log(array)
 
+
 for (var i = 1; i <= number; i++) {
-  array.push(i);
+  array.push(" " + i);
 };
 
 $("#result").text(array);
   });
 });
+
+
+// Attempts at getting the array into an ordered list, in js:
+
+// array.push(<ol> " " + i </ol>);
+
+// array += ("<ol>"  + array + "</ol>");
+
+// or in html
+
+// $("#result").text("<ol>"array"</ol>");
+
+// $("#result").text(<ol>array</ol>);
+
+
+
+
+// Attempts at for loops:
+
+//for (var i = 1; i <= number; i++) {
+//  if (i % 3 === 0){
+//  array.push(" " + "ping");
+//  } else if (i % 5 === 0){
+//    array.push(" " + "pong");
+//  } else if (i % 3 === 0 && i % 5 === 0);
+//    array.push(" " + "pingpong");
+//  } else
+//    array.push(" " + i);
+//  };
+
+
+//for (var i = 1; i <= number; i++) {
+  //if (i !% 3 === 0 && i !% 5 === 0){
+    //array.push(" " + i);
+  //} else if (i % 3 === 0){
+    //array.push(" " + "ping");
+  //} else if (i % 5 === 0){
+    //array.push(" " + "pong");
+  //} else if (i % 3 === 0 && i % 5 === 0);
+  //array.push(" " + "pingpong");
+//};
